89d8be0dfa813cb70ee9c0006ae42863cc6aa780
kurages <contact@kurages.dev>
2026-02-06 15:05:11 +0000

    fix build

--- packages/workers/src/nexus-register.ts
+++ packages/workers/src/nexus-register.ts
@@ -3,9 +3,10 @@ import { registry } from "./register";
 import { createManifest, InteractionHandlerRegistry } from "@hiyocord/hiyocord-nexus-core/src";
 
 
-const manifest = createManifest(registry as any as InteractionHandlerRegistry, {
+
+const manifest = createManifest(registry as InteractionHandlerRegistry, {
   baseUrl: "https://nexus-test.hiyocord.org",
-  id: "org.hiyocord.nexus-test",
+  id: "org.hiyocord.nexus-test1",
   name: "hiyocord nexus test service workers",
   description: "test",
   permissions: [{
@@ -14,20 +15,23 @@ const manifest = createManifest(registry as any as InteractionHandlerRegistry, {
       "/guilds/:guildId": ["GET"]
     }
   }],
-  signatureAlgorithm: "ed25519",
-  publicKey: ""
+  signatureAlgorithm: process.argv[2] as any,
+  publicKey: process.argv[3]
 })
 
-console.error(JSON.stringify(manifest))
-
+console.log(JSON.stringify(manifest, null, 2))
 const response = await fetch(`${process.env.NEXUS_URL}/api/manifests`, {
   method: "POST",
-  body: JSON.stringify(manifest)
+  body: JSON.stringify(manifest),
+  headers: {
+    "Content-Type": "application/json",
+  }
 })
 
 if(response.ok) {
   console.log(`OK: Nexus POST /manifest ${response.status}`)
 } else {
-  console.log(`Fail: Nexus POST /manifest ${response.status}`)
+  console.error(`Fail: Nexus POST /manifest ${response.status}`)
+  console.error(JSON.stringify(await response.json(), null, 2))
   exit(1)
 }
